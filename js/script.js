92483964e13ad6be6b3e8b770127baa950c63a95
Verta-master <vertyagina@gmail.com>
2020-12-16 17:30:17 +0600

    Update 2020-12-16T11:30:02.310Z

--- js/script.js
+++ js/script.js
@@ -46,3 +46,19 @@ $('.header__closebtn').click(function() {
   $('.header__droptablet').removeClass('header__droptablet--open');
   $('.overlay').removeClass('overlay--show');
 })
+
+//Scroll to top
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $(".btn-top").fadeIn();
+  } else {
+    $(".btn-top").fadeOut();
+  };
+});
+
+$(".btn-top").click(function () {
+  $("body,html").animate({
+    scrollTop: 0
+  }, 400);
+  return false;
+});
